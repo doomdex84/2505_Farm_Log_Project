41522f91dffa676cb121119f910d50ef8066ce9a
doomdex84 <doomdex84@gmail.com>
2025-05-08 11:57:06 +0900

    회원가입 기능 추가, 회원 테이블 및 TD 추가, 중복체크 x

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -16,16 +16,6 @@ public class ArticleService {
 
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
-		makeTestData();
-	}
-
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = "내용" + i;
-
-			articleRepository.writeArticle(title, body);
-		}
 
 	}
 
